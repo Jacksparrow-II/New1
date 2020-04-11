e171c6a08920828163405c280957b6c5a1162f69
Nisarg Prajapati <nprajapati@stridely.local>
2020-04-11 16:27:10 +0530

    Add project files.

---
+++ Controllers/empController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using New1.Models;
+
+namespace New1.Controllers
+{
+    public class empController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Addemp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Addemp(Employee emp)
+        {
+            Employee em = new Employee();
+                if (em.Addemp(emp))
+                {
+                    ViewBag.ErrMsg = "Employee Details are successfully Added";
+                }
+                else
+                {
+                    ViewBag.ErrMsg = "Employee Details are Not successfully Added";
+                }
+                return View(emp);
+        }
+
+        public IActionResult Getemp()
+        {
+            Employee sb = new Employee();
+            return View(sb.Getemp());
+        }
+
+        public IActionResult UpdateEmployee(int id)
+        {
+            Employee st = new Employee();
+            return View(st.Getemp().Find(asd => asd.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult UpdateEmployee(int ID, Employee s1)
+        {
+            Employee emp = new Employee();
+            if (emp.Updateemp(s1))
+            {
+                return RedirectToAction("Getemp");
+            }
+            else
+            {
+                return View();
+
+            }
+        }
+
+
+        public IActionResult DeleteEmployee(int ID)
+        {
+            Employee emp = new Employee();
+            if(emp.Deleteemp(ID))
+            {
+                return RedirectToAction("Getemp");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Listemp()
+        {
+            return View();
+        }
+    }
+}
+
+
---
+++ SQLQuery2.sql
@@ -0,0 +1,38 @@
+﻿create procedure AddEmployee(
+@Name VARCHAR(50),
+@empcode int,
+@salary Money,
+@Position varchar(100)
+
+)
+as
+begin
+insert into employee values(@Name,@empcode,@salary,@position);
+end
+
+create procedure Getemployee
+as
+begin
+Select * from employee;
+end
+
+create procedure UpdateEmployee(
+@id int,
+@Name VARCHAR(50),
+@empcode int,
+@salary Money,
+@Position varchar(100)
+
+)
+as
+begin
+Update employee set Name=@Name, empcode=@empcode, @salary=@salary, position=@Position where Id = @id;
+end
+
+create procedure DeleteEmp(
+@id int
+)
+as
+begin
+Delete From employee where Id = @id;
+end
